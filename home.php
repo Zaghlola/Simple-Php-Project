da960724facd35c6eb0d17a23456220dd92daca4
Zaghlola <zaatef12@gmail.com>
2022-05-26 03:27:11 +0200

    Simple Php Project

---
+++ home.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+  
+</head>
+<body>
+  <div id="main">
+      <form action="" method="post">
+          <aside>
+            <div id="lside">
+                <img src="" alt="logo" srcset="">
+                <h3>School Logo</h3>
+
+                <label for="id">id</label><br>
+                <input type="number" name="id"><br>
+
+                <label for="name">Name</label><br>
+                <input type="text" name="name"><br>
+
+                <label for="address">Address</label><br>
+                <input type="text" name="address"><br><br>
+                
+                <button name="add">Add</button>
+                <button name="delete">Delete</button>
+
+
+
+            </div>
+          </aside>
+          <main>
+              <table>
+                <tr>
+                  <th>   Id      </th>
+                  <th>   Name    </th>
+                  <th>   Address </th>
+                </tr>
+                
+              </table>
+          </main>
+      </form>
+  </div>  
+</body>
+</html>
